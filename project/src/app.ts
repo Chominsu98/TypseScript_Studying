88d66188ce77fa5eb166d4606eb5b97aae8a7c52
ChoMinsu <plugnplay98@naver.com>
2022-09-07 14:21:50 +0900

    프로젝트 설정tsconfig

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,5 +1,3 @@
-//@ts-check
-
 // utils
 function $(selector) {
   return document.querySelector(selector);
@@ -38,21 +36,11 @@ function createSpinnerElement(id) {
 let isDeathLoading = false;
 let isRecoveredLoading = false;
 
-
-/**
- * @typedef {object} CovidSummary
- * @property {Array<object>} Country
- */
 // api
-/**
- * 
- * @returns {Promise<CovidSummary>}
- */
 function fetchCovidSummary() {
   const url = 'https://api.covid19api.com/summary';
   return axios.get(url);
 }
-
 
 function fetchCountryInfo(countryCode, status) {
   // params: confirmed, recovered, deaths
